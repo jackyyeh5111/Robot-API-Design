8017ac9390d1b786ac59f6f09fe6317a9962c2ee
Jacky Yeh <jackyyeh511@gmail.com>
2024-12-22 19:45:58 -0600

    integrate visualier into Robot

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from models import Point, Grid, Robot
 from path_planning import PathPlannerFactory
+import argparse
+import yaml
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('--config_path', '-c', type=str, required=True)
+    parser.add_argument('--is_vis', '-v', action='store_true',
+                        help='visualize robot navigation process')
+    args = parser.parse_args()
+
+    # Reading a YAML file
+    # with open("config.yaml", "r") as file:
+    #     config = yaml.safe_load(file)
+
+    # print(config)
+
     start_pt = Point(0, 0)
     end_pt = Point(2, 2)
     width = 3
     height = 3
     grid = Grid(width, height)
-    robot = Robot(start_pt, grid)
-    
+    robot = Robot(start_pt, grid, args.is_vis)
+
     planner_type = "dijkstra"  # Could be "astar" or "bfs"
     planner = PathPlannerFactory.get_planner(planner_type)
     print(f"Using planner: {planner.__class__.__name__}")
 
     robot.navigate(end_pt, planner)
-    
+
+
 if __name__ == '__main__':
     main()
--- models/robot.py
+++ models/robot.py
@@ -2,12 +2,40 @@ from typing import List, Tuple
 from .point import Point
 from .grid import Grid
 from path_planning import PathPlanner
+import os
+import time
 
+class Visualizer:
+    def __init__(self, grid: Grid):
+        self.grid = grid
 
+    def reset(self):
+        self.canvas = [['-' for _ in range(self.grid.width)]
+                       for _ in range(self.grid.height)]
+
+        # draw obstacles
+        for pt in self.grid.obstacles:
+            self.vis_grid[pt.y][pt.x] = 'X'
+
+    def update(self, robot_pos: Point, delay: float = 1.):
+        # Clear the console
+        os.system('cls' if os.name == 'nt' else 'clear')
+        self.reset()
+
+        # Update robot's position
+        self.canvas[robot_pos.y][robot_pos.x] = 'R'
+
+        # Display the grid
+        for row in self.canvas:
+            print(' '.join(row))
+        
 class Robot:
-    def __init__(self, cur_pt: Point, grid: Grid):
+    def __init__(self, cur_pt: Point, grid: Grid, is_vis: bool):
         self.cur_pt = cur_pt
         self.grid = grid
+        self.is_vis = is_vis
+        if self.is_vis:
+            self.visualizer = Visualizer(self.grid)
 
     def navigate(self, end_pt: Point, path_planner: PathPlanner) -> bool:
         if not self.grid.is_valid_point(end_pt):
@@ -15,12 +43,24 @@ class Robot:
 
         path = path_planner.plan(self.cur_pt, end_pt, self.grid)
 
+        if self.is_vis:
+            self.visualizer.update(self.cur_pt)
+                
         # Control the robot's movement
         for next_pt in path:
             if self.cur_pt == next_pt:
                 continue
             direction = next_pt - self.cur_pt
+                
+            if self.is_vis:
+                self.visualizer.update(self.cur_pt + direction)
+                
             self.move(direction)
+            
+            # keep display frame for 1 sec
+            if self.is_vis:
+                time.sleep(1.0)
+            
         return True
 
     def move(self, direction: Point):
